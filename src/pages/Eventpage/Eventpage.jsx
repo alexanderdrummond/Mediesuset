0264e0c89569d875abd4806d206f0c7f566c9236
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2024-01-31 04:31:12 -0600

    Artist modal added (data display only)

--- src/pages/Eventpage/Eventpage.jsx
+++ src/pages/Eventpage/Eventpage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EventBar } from "../../components/EventBar/EventBar";
 import { EventCard } from "../../components/EventCard/EventCard";
 import { Title } from "../../components/Title/Title";
+import { Modal } from "../../components/Modal/Modal";
 import { getDayFromDate } from "../../helpers/getDayFromDate";
 import { getEventColor } from "../../helpers/getEventColor";
 import { useFetch } from "../../hooks/useFetch";
@@ -9,9 +10,10 @@ import style from "../Eventpage/Eventpage.module.scss";
 
 export const Eventpage = () => {
   const events = useFetch("https://api.mediehuset.net/mediesuset/events");
-  const [selectedEvent, setSelectedEvent] = useState("0");
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [selectedEventID, setSelectedEventID] = useState(null); 
+  const eventDetails = useFetch(`https://api.mediehuset.net/mediesuset/events/${selectedEventID}`);
 
-  console.log(selectedEvent);
   const daysInWeek = [
     "Mandag",
     "Tirsdag",
@@ -22,7 +24,12 @@ export const Eventpage = () => {
     "Søndag",
   ];
 
-  console.log(events);
+  const [selectedEvent, setSelectedEvent] = useState("0");
+
+  const handleModal = (id) => {
+    setSelectedEventID(id);
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <>
@@ -30,24 +37,41 @@ export const Eventpage = () => {
       <EventBar setSelectedEvent={setSelectedEvent} />
       <section className={style.eventPageWrapper}>
         {events ? (
-          events?.items.map((item) => {
-            if (item.stage_id === selectedEvent || selectedEvent === "0")
+          events.items.map((item) => {
+            if (item.stage_id === selectedEvent || selectedEvent === "0") {
               return (
+                <div className="event-card" onClick={() => handleModal(item.id)}>
                 <EventCard
                   key={item.id}
                   title={item.title}
                   image={item.image}
-                  date={`${
-                    daysInWeek[getDayFromDate(item.local_time)]
-                  } kl. ${item.local_time.substring(11, 16)}`}
+                  date={`${daysInWeek[getDayFromDate(item.local_time)]} kl. ${item.local_time.substring(11, 16)}`}
                   stageColor={getEventColor(item.stage_id)}
+                 
                 />
+                </div>
               );
+            }
+            return null; 
           })
         ) : (
           <h3>Could not load events - try again</h3>
         )}
       </section>
+      {isModalOpen && (
+        <Modal handleModal={() => handleModal(null)} isModalOpen={isModalOpen}>
+        
+          <figure className={style.eventDetailsWrapper}>
+            <img src={eventDetails?.item.image} alt={eventDetails?.item.title} />
+            <figcaption>
+              <h4>{eventDetails?.item.title}</h4>
+              <p>{eventDetails?.item.description}</p>
+              <p>{`${eventDetails?.item.stage_name}`}</p>
+              <p>{`${new Date(eventDetails?.item.datetime * 1000).toString()}`}</p>
+            </figcaption>
+          </figure>
+        </Modal>
+      )}
     </>
   );
 };
