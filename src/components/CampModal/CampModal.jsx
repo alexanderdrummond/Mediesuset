fe8a10973a4715b5124b2a16ab90c161831bf1dc
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2024-02-01 13:27:52 +0100

    Updated modal

--- src/components/CampModal/CampModal.jsx
+++ src/components/CampModal/CampModal.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
-import style from './CampModal.module.scss';
+import React from "react";
+import style from "./CampModal.module.scss";
 
 const CampModal = ({ camp, onClose }) => {
-  
-  const partoutTickets = camp.tickets.filter(ticket => ticket.type === 'partout');
-  const singleTickets = camp.tickets.filter(ticket => ticket.type === 'single');
+  const partoutTickets = camp.tickets.filter(
+    (ticket) => ticket.type === "partout"
+  );
+  const singleTickets = camp.tickets.filter(
+    (ticket) => ticket.type === "single"
+  );
 
-
-  const renderTicketCards = (tickets) => tickets.map(ticket => (
-    <div key={ticket.id} className={style.ticketCard}>
-      <h3>{ticket.name}</h3>
-      <p>{ticket.description}</p>
-      <p>Price: {ticket.price} DKK</p>
-    </div>
-  ));
+  const renderTicketCards = (tickets) =>
+    tickets.map((ticket) => (
+      <div key={ticket.id} className={style.ticketCard}>
+        <h3>{ticket.name}</h3>
+        <p>{ticket.description}</p>
+        <p>Pris: {ticket.price} DKK</p>
+      </div>
+    ));
 
   return (
     <div className={style.modalBackground}>
       <div className={style.modalContent}>
-        <button className={style.closeButton} onClick={onClose}>X</button>
+        <button className={style.closeButton} onClick={onClose}>
+          X
+        </button>
         <img src={camp.image} alt={camp.name} />
         <h2>{camp.name}</h2>
         <p>Kapacitet: {camp.num_people}</p>
         <p> {camp.description}</p>
-        
+
         <h3>Adgangsgivende Billetter</h3>
 
         {partoutTickets.length > 0 && (
